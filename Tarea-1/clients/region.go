92193d020394914a833ec4ec5c5b3035f7e10da0
Rak3z <javier.antonio.perez.pinto@gmail.com>
2023-09-11 13:06:25 -0300

    while y wea de rbmq

--- Tarea-1/clients/region.go
+++ Tarea-1/clients/region.go
@@ -18,11 +18,10 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
     var interesed_users int64
-	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
 
-	var f, errar = os.Open("parametros_de_inicio.txt")
-    if errar != nil {
-        log.Fatal(errar)
+	var f, ar_err = os.Open("parametros_de_inicio.txt")
+    if ar_err != nil {
+        log.Fatal(ar_err)
     }
     var fileScanner = bufio.NewScanner(f)
     fileScanner.Split(bufio.ScanLines)
@@ -34,49 +33,70 @@ func main() {
         }
         interesed_users += val
     }
-    
-    twtpercent := float64(interesed_users) / 2 * 0.2
-    lower_int := int64(float64(interesed_users)/2 - twtpercent)
-    upper_int := int64(float64(interesed_users)/2 + twtpercent)
- 	SolicitedKeys := rand.Int63n(upper_int-lower_int) + lower_int
-
- 	fmt.Println("Número aleatorio:", SolicitedKeys)
-    fmt.Println("Límite inferior:", lower_int)
-    fmt.Println("Límite superior:", upper_int)
-/*
-el servidor regional deber´a generar un valor entre la
-cantidad de usuarios interesados que contiene en su archivo ”parametros de inicio”
-dividida en 2 menos el 20% de ese valor y la misma cantidad dividida en 2 m´as el
-20% del valor. Es decir, si dentro del archivo ”parametros de inicio” el servidor
-regional tiene una cantidad de 300 interesados, deber´a generar un n´umero al azar
-entre 120 y 180.*/
+    while(interesed_users > 0){
+	    twtpercent := float64(interesed_users) / 2 * 0.2
+	    lower_int := int64(float64(interesed_users)/2 - twtpercent)
+	    upper_int := int64(float64(interesed_users)/2 + twtpercent)
+	 	SolicitedKeys := rand.Int63n(upper_int-lower_int) + lower_int
 
 
-	if err != nil {
-		log.Fatal("Can't connect to server: %v", err)
+
+	 	//recieve avaible keys
+		conn, con_err := grpc.Dial(":50051", grpc.WithInsecure())
+		if con_err != nil {
+			log.Fatal("Can't connect to server: %v", con_err)
+		}
+
+		serviceClient := pb.NewNotifyKeysClient(conn)
+		res, l_err := serviceClient.SendKeys(context.Background(), &pb.AvailableKeysReq{
+			Id:  1,
+			Qty: 5,
+		})
+		if l_err != nil {
+			log.Fatal("Keys are not created" + l_err.Error())
+		}
+		fmt.Println("Llaves disponibles: " + strconv.FormatInt(res.Qty, 10))
+		//rabbitmq queue
+		// Connect with Rabbit Queue
+		rabbit_conn, rabbit_err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+		if rabbit_err != nil {
+			fmt.Println(rabbit_err)
+			panic(err)
+		}
+
+		ch, rab_con_err := rabbit_conn.Channel()
+		if rab_con_err != nil {
+			fmt.Println(rab_con_err)
+		}
+
+		defer ch.Close()
+
+		msgs, send_mq_err := ch.Publish(
+			"TestQueue",
+			"test",   			//chantar nombre de sevidor regional
+			SolicitedKeys, 		//numero de keys a solicitar
+			true,
+			false,
+			false,
+			false,
+			nil,
+		)
+
+		if send_mq_err != nil {
+			fmt.Println(send_mq_err)
+		}
+
+		// notification part
+		serviceClient2 := pb.NewFinalNotificationClient(conn)
+		res2, err2 := serviceClient2.NotifyRegional(context.Background(), &pb.FinalNotifyRequest{
+			NumberOfUsersFailed: int32(1),
+		})
+		if err2 != nil {
+			log.Fatalf("Failed to notify regional server: %v", err2)
+		}
+
+		interesed_users -= (SolicitedKeys - res2.Message)
+		log.Printf("Regional server response: %s", res2.Message)
 	}
-
-	serviceClient := pb.NewNotifyKeysClient(conn)
-
-	res, l_err := serviceClient.SendKeys(context.Background(), &pb.AvailableKeysReq{
-		Id:  1,
-		Qty: 5,
-	})
-
-	if l_err != nil {
-		log.Fatal("Keys are not created" + l_err.Error())
-	}
-
-	fmt.Println("Llaves recibidas: " + strconv.FormatInt(res.Qty, 10))
-
-	// Parte de notificación
-	serviceClient2 := pb.NewFinalNotificationClient(conn)
-	res2, err2 := serviceClient2.NotifyRegional(context.Background(), &pb.FinalNotifyRequest{
-		NumberOfUsersFailed: int32(1),
-	})
-	if err2 != nil {
-		log.Fatalf("Failed to notify regional server: %v", err2)
-	}
-
-	log.Printf("Regional server response: %s", res2.Message)
 }
