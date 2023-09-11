c35db5db56c7b9f173aaf5960b36b02043360060
Rak3z <javier.antonio.perez.pinto@gmail.com>
2023-09-11 11:30:24 -0300

    wea del archivo region.go

--- Tarea-1/clients/region.go
+++ Tarea-1/clients/region.go
@@ -2,16 +2,55 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"strconv"
+    "bufio"
+    "fmt"
+    "log"
+    "math/rand"
+    "os"
+    "strconv"
+    "time"
 
 	pb "github.com/sofiwiwiwi/2023_1-Distro/tree/bup-develop/Tarea-1/protofiles"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+    var interesed_users int64
 	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+
+	var f, errar = os.Open("parametros_de_inicio.txt")
+    if errar != nil {
+        log.Fatal(errar)
+    }
+    var fileScanner = bufio.NewScanner(f)
+    fileScanner.Split(bufio.ScanLines)
+ 	for fileScanner.Scan() {
+        text := fileScanner.Text()
+        val, err := strconv.ParseInt(text, 10, 64)
+        if err != nil {
+            log.Fatalf("Error al analizar la línea %s: %v", text, err)
+        }
+        interesed_users += val
+    }
+    
+    twtpercent := float64(interesed_users) / 2 * 0.2
+    lower_int := int64(float64(interesed_users)/2 - twtpercent)
+    upper_int := int64(float64(interesed_users)/2 + twtpercent)
+ 	SolicitedKeys := rand.Int63n(upper_int-lower_int) + lower_int
+
+ 	fmt.Println("Número aleatorio:", SolicitedKeys)
+    fmt.Println("Límite inferior:", lower_int)
+    fmt.Println("Límite superior:", upper_int)
+/*
+el servidor regional deber´a generar un valor entre la
+cantidad de usuarios interesados que contiene en su archivo ”parametros de inicio”
+dividida en 2 menos el 20% de ese valor y la misma cantidad dividida en 2 m´as el
+20% del valor. Es decir, si dentro del archivo ”parametros de inicio” el servidor
+regional tiene una cantidad de 300 interesados, deber´a generar un n´umero al azar
+entre 120 y 180.*/
+
 
 	if err != nil {
 		log.Fatal("Can't connect to server: %v", err)
