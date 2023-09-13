55dc61296d2dc682476315c6a84242764fee9cd3
Bibubobee <gaboenix.2@gmail.com>
2023-09-12 22:42:03 -0300

    center 99% finished

--- Tarea-1/center.go
+++ Tarea-1/center.go
@@ -19,7 +19,7 @@ import (
 
 var register_f, register_err = os.Create("registro_flujo.txt")
 var conn_asia, conn_america, conn_europe, conn_oceania *grpc.ClientConn
-
+var available [4]bool = [4]bool{true, true, true, true}
 var users_left bool = true
 
 func receive_from_mq(msgs <-chan amqp.Delivery) {
@@ -72,65 +72,54 @@ func send_keys_to_all(keys int32) {
 	}
 	var clients = [4]*grpc.ClientConn{conn_asia, conn_europe, conn_oceania, conn_america}
 
-	for _, conn := range clients {
+	for index, conn := range clients {
+		if !available[index] {
+			continue
+		}
 		this_client := pb.NewNotifyKeysClient(conn)
 		_, l_client_err := this_client.SendKeys(context.Background(), &pb.AvailableKeysReq{
 			Keys: keys, // reemplazar con keys
 		})
 		fmt.Println("Enviado")
 		if l_client_err != nil {
-			log.Fatal("Couldn't send keys to Server: ", l_client_err)
+			log.Fatal("Couldn't send keys to region: ", l_client_err)
 		}
 	}
-
-	// BORRAR LUEGO DE TESTEAR
-	// asia_client := pb.NewNotifyKeysClient(conn_asia)
-	// _, l_asia_err := asia_client.SendKeys(context.Background(), &pb.AvailableKeysReq{
-	// 	Id:  1,
-	// 	Qty: 5,
-	// })
-	// if l_asia_err != nil {
-	// 	log.Fatal("Couldn't send keys to Asia: ", l_asia_err)
-	// }
-
-	// europe_client := pb.NewNotifyKeysClient(conn_europe)
-	// _, l_europe_err := europe_client.SendKeys(context.Background(), &pb.AvailableKeysReq{
-	// 	Id:  1,
-	// 	Qty: 5,
-	// })
-	// if l_europe_err != nil {
-	// 	log.Fatal("Couldn't send keys to Europe: ", l_europe_err)
-	// }
-
-	// oceania_client := pb.NewNotifyKeysClient(conn_oceania)
-	// _, l_oceania_err := oceania_client.SendKeys(context.Background(), &pb.AvailableKeysReq{
-	// 	Id:  1,
-	// 	Qty: 5,
-	// })
-	// if l_oceania_err != nil {
-	// 	log.Fatal("Couldn't send keys to Asia: ", l_oceania_err)
-	// }
-
-	// america_client := pb.NewNotifyKeysClient(conn_america)
-	// _, l_america_err := america_client.SendKeys(context.Background(), &pb.AvailableKeysReq{
-	// 	Id:  1,
-	// 	Qty: 5,
-	// })
-	// if l_america_err != nil {
-	// 	log.Fatal("Couldn't send keys to Asia: ", l_america_err)
-	// }
 }
 
 func notify_continue_to_all() {
 	var clients = [4]*grpc.ClientConn{conn_asia, conn_europe, conn_oceania, conn_america}
 
-	for _, conn := range clients {
+	for index, conn := range clients {
+		if !available[index] {
+			continue
+		}
 		this_client := pb.NewNotifyKeysClient(conn)
-		_, l_client_err := this_client.NotifyContinue(context.Background(), &pb.ContinueServiceReq{
+		res, l_client_err := this_client.NotifyContinue(context.Background(), &pb.ContinueServiceReq{
 			Continue: users_left, // reemplazar con keys
 		})
 		if l_client_err != nil {
-			log.Fatal("Couldn't send confirm to server: ", l_client_err)
+			log.Fatal("Couldn't send confirm to region: ", l_client_err)
+		}
+		available[index] = res.Continue
+		conn.Close()
+	}
+}
+
+func notify_users_left_to_all(requested []int32, assigned []int32, servers []string) {
+	var clients = [4]*grpc.ClientConn{conn_asia, conn_europe, conn_oceania, conn_america}
+	for index, conn := range clients {
+		if !available[index] {
+			continue
+		}
+		this_client := pb.NewNotifyKeysClient(conn)
+		fmt.Println("Se inscribieron ", assigned[index], "cupos de servidor ", servers[index])
+		var not_registered int32 = requested[index] - assigned[index]
+		_, l_client_err := this_client.UsersNotAdmittedNotify(context.Background(), &pb.UsersNotAdmittedReq{
+			Users: not_registered,
+		})
+		if l_client_err != nil {
+			log.Fatal("Couldn't send users left to region: ", l_client_err)
 		}
 		conn.Close()
 	}
@@ -230,7 +219,7 @@ func main() {
 		// Receive user peticions
 		// receive_from_mq(msgs)
 
-		// Notify how many didn't get in
+		notify_users_left_to_all()
 
 		// Notify Continue
 		connect_to_all()
