4e6608f8a50c3bbd5c15c68c67add91b0c820d6f
Rak3z <javier.antonio.perez.pinto@gmail.com>
2023-09-13 00:04:01 -0300

    aplicados cambios de regional en cada región

--- Tarea-1/clients/asia/asia.go
+++ Tarea-1/clients/asia/asia.go
@@ -19,6 +19,7 @@ import (
 var serv *grpc.Server
 var keep_iterating bool = true
 var users_left int32
+var interested_users_global int32
 
 type server struct {
 	pb.UnimplementedNotifyKeysServer
@@ -35,7 +36,7 @@ func (s *server) SendKeys(ctx context.Context, req *pb.AvailableKeysReq) (*pb.Em
 }
 
 func (s *server) NotifyContinue(ctx context.Context, req *pb.ContinueServiceReq) (*pb.ContinueServiceReq, error) {
-	keep_iterating = req.Continue && users_left != 0
+	keep_iterating = req.Continue && interested_users_global > 0
 	fmt.Println("Continue?: ", keep_iterating)
 	go func() {
 		time.Sleep(1 * time.Second)
@@ -43,6 +44,17 @@ func (s *server) NotifyContinue(ctx context.Context, req *pb.ContinueServiceReq)
 	}()
 	return &pb.ContinueServiceReq{Continue: keep_iterating}, nil
 }
+
+func (s *server) UsersNotAdmittedNotify(ctx context.Context, req *pb.UsersNotAdmittedReq) (*pb.Empty, error) {
+	users_left = req.Users
+	fmt.Println("usuarios sin key?: ", req.Users)
+	go func() {
+		time.Sleep(1 * time.Second)
+		serv.Stop()
+	}()
+	return &pb.Empty{}, nil
+}
+
 func start_grpc_server() {
 	// Establish grpc connection.
 	listner, s_err := net.Listen("tcp", ":50053")
@@ -63,7 +75,7 @@ func start_grpc_server() {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	var interesed_users int64
+	var interesed_users int32
 
 	var f, ar_err = os.Open("clients/parametros_de_inicio.txt")
 	if ar_err != nil {
@@ -77,61 +89,58 @@ func main() {
 		if err != nil {
 			log.Fatalf("Error al analizar la línea %s: %v", text, err)
 		}
-		interesed_users += val
+		interesed_users = int32(val)
 	}
-	for interesed_users > 0 && keep_iterating {
-		start_grpc_server() // Wait for keys received
 
+	// rabbit_conn, rabbit_err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	// if rabbit_err != nil {
+	// 	fmt.Println(rabbit_err)
+	// 	panic(rabbit_err)
+	// }
+
+	// ch, rab_con_err := rabbit_conn.Channel()
+	// if rab_con_err != nil {
+	// 	fmt.Println(rab_con_err)
+	// }
+
+	// defer ch.Close()
+	// queueName := "TestQueue"
+
+	for interesed_users > 0 && keep_iterating {
+
+		start_grpc_server() // Wait for keys received
 		twtpercent := float64(interesed_users) / 2 * 0.2
 		lower_int := int64(float64(interesed_users)/2 - twtpercent)
 		upper_int := int64(float64(interesed_users)/2 + twtpercent)
 		SolicitedKeys := rand.Int63n(upper_int-lower_int) + lower_int
 		fmt.Println("Solicited Keys: ", SolicitedKeys)
-
-		// RABBIT
-
-		start_grpc_server() // Wait for NotifyContinue
-		//rabbitmq queue
-		// Connect with Rabbit Queue
-		// rabbit_conn, rabbit_err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-
-		// if rabbit_err != nil {
-		// 	fmt.Println(rabbit_err)
-		// 	panic(rabbit_err)
-		// }
-
-		// ch, rab_con_err := rabbit_conn.Channel()
-		// if rab_con_err != nil {
-		// 	fmt.Println(rab_con_err)
-		// }
-
-		// defer ch.Close()
-
-		// msgs, send_mq_err := ch.Publish(
-		// 	"TestQueue",
-		// 	"asia",        //chantar nombre de sevidor regional
-		// 	SolicitedKeys, //numero de keys a solicitar
-		// 	true,
+		// messageBody := fmt.Sprintf("asia,%d", SolicitedKeys)
+		// send_mq_err := ch.Publish(
+		// 	"",
+		// 	queueName,
 		// 	false,
 		// 	false,
-		// 	false,
-		// 	nil,
+		// 	amqp.Publishing{
+		// 		ContentType: "text/plain",
+		// 		Body:        []byte(messageBody),
+		// 	},
 		// )
 
-		// if send_mq_err != nil {
-		// 	fmt.Println(send_mq_err)
-		// }
+		// if sen_mq_err != nil {
+		// 	log.Printf("no se publicó el mensaje: %v", send_mq_err)
+		// } else {
+		// 	log.Printf("se publicó el mensaje: %s", messageBody)
+		// }d
 
-		// // notification part
-		// serviceClient2 := pb.NewFinalNotificationClient(conn)
-		// res2, err2 := serviceClient2.NotifyRegional(context.Background(), &pb.FinalNotifyRequest{
-		// 	NumberOfUsersFailed: int32(1),
-		// })
-		// if err2 != nil {
-		// 	log.Fatalf("Failed to notify asia server: %v", err2)
-		// }
+		start_grpc_server() // Wait for UsersNotAdmittedNotify
+		enrolled_users := (int32(SolicitedKeys) - users_left)
+		interesed_users -= enrolled_users
+		fmt.Println("Se inscribieron", enrolled_users, "personas")
+		interested_users_global = interesed_users
+		fmt.Println("Quedan", interested_users_global, "personas en espera de cupo")
+		// log.Printf("Regional server response: %s", NumberUsersFailed)
 
-		// interesed_users -= (SolicitedKeys - res2.Message)
-		// log.Printf("asia server response: %s", res2.Message)
+		start_grpc_server() // Wait for NotifyContinue
 	}
 }
